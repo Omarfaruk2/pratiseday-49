71418e106b46ed71b21f411c768e8ec2180695fe
Omarfaruk4200 <emon35293@gmail.com>
2022-03-24 23:01:07 +0600

    added

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -28,7 +28,7 @@ const Header = () => {
                                 <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
                                 <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
                                 <NavDropdown.Divider />
-                                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                                <NavDropdown.Item href="#action/3.4">Separated lnk</NavDropdown.Item>
                             </NavDropdown>
                         </Nav>
                         <Nav>
